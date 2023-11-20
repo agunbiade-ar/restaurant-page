0f42b576bc84a09d96f5632fbc0c0ceaa4fc832f
Agunbiade Abdulrasheed <79267850+agunbiade-ar@users.noreply.github.com>
2023-11-20 20:58:57 +0100

    second commit: moved code from src/index.js to src/manipulate.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
 import './styles/styles.css';
-
-function openTab(e, tabName) {
-  let i, tabcontent, tablinks;
-
-  tabcontent = document.getElementsByClassName('tabcontent');
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = 'none';
-  }
-
-  tablinks = document.getElementsByClassName('tablinks');
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(' active', '');
-  }
-  console.log(tabName);
-  document.querySelector(`#${tabName}`).style.display = 'block';
-  e.currentTarget.className += ' active';
-}
+import openTab from './manipulateTabs';
 
 document.addEventListener('DOMContentLoaded', function () {
   const tabButtons = document.querySelectorAll('.tablinks');
